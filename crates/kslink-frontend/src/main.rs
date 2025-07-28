35b9739372462f1ac62d701d5629f150403a26a2
Krysztal Huang <krysztal.huang@outlook.com>
2025-07-29 00:14:30 +0800

    feat(kslink-frontend): implemented basic layout of frontend

--- crates/kslink-frontend/src/main.rs
+++ crates/kslink-frontend/src/main.rs
@@ -1,18 +1,19 @@
 use dioxus::prelude::*;
 
+mod component;
+
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 enum Route {
-    #[layout(Navbar)]
+    #[layout(BaseLayout)]
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
+    #[route("/statistics")]
+     Statistics {},
 }
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -22,26 +23,11 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Router::<Route> {}
-    }
-}
-
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+        div { class: "bg-primary duration-150",
+            document::Link { rel: "icon", href: FAVICON }
+            document::Link { rel: "stylesheet", href: MAIN_CSS }
+            document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+            Router::<Route> {}
         }
     }
 }
@@ -49,53 +35,29 @@ pub fn Hero() -> Element {
 /// Home page
 #[component]
 fn Home() -> Element {
-    rsx! {
-        Hero {}
-
-    }
+    rsx! {}
 }
 
-/// Blog page
 #[component]
-pub fn Blog(id: i32) -> Element {
-    rsx! {
-        div {
-            id: "blog",
-
-            // Content
-            h1 { "This is blog #{id}!" }
-            p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
-
-            // Navigation links
-            Link {
-                to: Route::Blog { id: id - 1 },
-                "Previous"
-            }
-            span { " <---> " }
-            Link {
-                to: Route::Blog { id: id + 1 },
-                "Next"
-            }
-        }
-    }
+pub fn Statistics() -> Element {
+    rsx! {}
 }
 
-/// Shared navbar component.
 #[component]
-fn Navbar() -> Element {
+pub fn BaseLayout() -> Element {
     rsx! {
         div {
-            id: "navbar",
-            Link {
-                to: Route::Home {},
-                "Home"
-            }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
-        }
-
-        Outlet::<Route> {}
+            component::NavBar {
+                title: "KSLink",
+                links: vec![
+                    (String::from("Home"),String::from("/")),
+                    (String::from("Statistics"),String::from("")),
+                ]
+            },
+        },
+        div { class:"fixed z-10 min-h-full min-w-full bg-base-100",
+            Outlet::<Route> {}
+        },
+        div { }
     }
 }
