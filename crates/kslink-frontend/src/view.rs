ef3b6ae778a3c917413c877bb4bf73ff3de4fa70
Krysztal Huang <krysztal.huang@outlook.com>
2025-07-30 22:17:38 +0800

    feat(kslink-frontend): implemented statistics page

--- crates/kslink-frontend/src/view.rs
+++ crates/kslink-frontend/src/view.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 
-use crate::component::UrlInputBox;
+use crate::component::{StatisticsItem, StatisticsList, UrlInputBox};
 
 #[component]
 pub fn Home() -> Element {
@@ -23,15 +23,44 @@ pub fn Home() -> Element {
 
 #[component]
 pub fn Statistics() -> Element {
+    let vec: Vec<(String, String, String, Element)> = vec![(
+        "Recorded".to_owned(),
+        "100000".to_owned(),
+        "links into database".to_owned(),
+        rsx! {
+            svg {
+                class: "size-6",
+                fill: "none",
+                stroke: "currentColor",
+                stroke_width: "1.5",
+                view_box: "0 0 24 24",
+                xmlns: "http://www.w3.org/2000/svg",
+                path {
+                    d: "m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z",
+                    stroke_linecap: "round",
+                    stroke_linejoin: "round",
+                }
+            }
+        },
+    )];
+
     rsx! {
         div {
             class: "hero bg-base-200 min-h-screen",
             div {
-                class: "hero-content text-center text-base-content",
+                class: "hero-content text-base-content flex-col py-12",
                 div {
-                    class: "max-w-max",
-                    h1 { class: "text-5xl font-bold m-12",
-                        "Simply shorten your link"
+                    class: "max-w-max text-center",
+                    h1 { class: "text-5xl font-bold m-12", "Statistics of this short link service" }
+                }
+                StatisticsList {
+                    for (title, value, desc , icon) in vec {
+                        StatisticsItem {
+                            title: title,
+                            value: value,
+                            icon: icon,
+                            desc: desc,
+                        }
                     }
                 }
             }
