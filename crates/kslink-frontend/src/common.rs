904070948959224794586c1afecd5b5685cd8da1
Krysztal Huang <krysztal.huang@outlook.com>
2025-08-03 23:05:14 +0800

    feat(kslink-frontend): implemented URL check

--- crates/kslink-frontend/src/common.rs
+++ crates/kslink-frontend/src/common.rs
@@ -1,2 +1,13 @@
+use std::str::FromStr;
+
+use url::Url;
+
 pub const BASE_URL: &str = env!("KSLINK_BASE_URL");
 pub const BUILD_TIME: &str = env!("BUILD_TIME");
+
+pub fn is_valid_url<T>(input: T) -> bool
+where
+    T: ToString,
+{
+    Url::from_str(&input.to_string()).is_ok()
+}
