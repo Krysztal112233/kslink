161500e59a0c25131fc0137dca63f628a7e4cc5b
Krysztal Huang <krysztal.huang@outlook.com>
2025-08-03 18:57:01 +0800

    feat(kslink-frontend): now `kslink-frontend`s base url controlled by environment `KSLINK_BASE_URL`

---
+++ crates/kslink-frontend/build.rs
@@ -0,0 +1,10 @@
+const ENV: &str = "KSLINK_BASE_URL";
+
+fn main() {
+    println!("cargo::rerun-if-env-changed=BUILD_TIME");
+
+    match std::env::var(ENV) {
+        Ok(env) => println!("cargo::rustc-env={ENV}={env}"),
+        Err(_) => println!("cargo::rustc-env={ENV}=http://127.0.0.1:8000"),
+    }
+}
---
+++ crates/kslink-frontend/src/common.rs
@@ -0,0 +1,1 @@
+pub const BASE_URL: &str = env!("KSLINK_BASE_URL");
