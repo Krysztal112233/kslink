5aa5e6b2d0f3deb7402f8568ec8ffe8d43fda863
Krysztal Huang <krysztal.huang@outlook.com>
2025-08-03 22:57:24 +0800

    feat(kslink-frontend): implemented `create` request for frontend

---
+++ crates/kslink-frontend/src/error.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+use url::Url;
+
+pub(crate) type Result<T> = ::std::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("internal error: {0}")]
+    Internal(String),
+
+    #[error("invalid url: {0}")]
+    InvalidUrl(Url),
+
+    #[error("unknown error: {0}")]
+    Unknown(String),
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Internal(format!("{value}"))
+    }
+}
--- crates/kslink-frontend/src/request.rs
+++ crates/kslink-frontend/src/request.rs
@@ -1,17 +1,55 @@
+use dioxus_logger::tracing;
 use reqwest::Client;
+use serde_json::Value;
 use url::Url;
 
-use crate::common;
+use crate::{common, error};
 
 #[derive(Debug)]
 pub struct Requester {
     client: Client,
 }
 
+#[allow(unused)]
 impl Requester {
-    async fn create(&self, from: Url) {
-        let url = common::BASE_URL;
+    pub fn new() -> Self {
+        Self {
+            client: Client::builder()
+                .user_agent("kslink/frontend")
+                .build()
+                .unwrap(),
+        }
+    }
 
-        let result = self.client.post(url).query(&("url", from)).send().await;
+    pub async fn create(&self, from: Url) -> error::Result<String> {
+        let url = common::BASE_URL;
+        let res = self
+            .client
+            .post(url)
+            .query(&("url", from))
+            .send()
+            .await
+            .inspect_err(|err| tracing::error!("{err}"))?;
+
+        if res.status().is_success() {
+            let res = res
+                .json::<Value>()
+                .await
+                .inspect_err(|err| tracing::error!("{err}"))?;
+
+            Ok(res["hash"]
+                .as_str()
+                .ok_or(error::Error::Unknown("unknow format".to_string()))?
+                .to_string())
+        } else {
+            Err(error::Error::Unknown(format!(
+                "server status {}",
+                res.status().as_u16()
+            )))
+        }
+    }
+
+    pub async fn statistics(&self) -> error::Result<()> {
+        todo!()
     }
 }
