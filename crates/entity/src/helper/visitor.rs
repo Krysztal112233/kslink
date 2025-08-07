f9cda7b6f2d165571377ab2b3d022e70c8143de7
Krysztal Huang <krysztal.huang@outlook.com>
2025-08-07 13:41:48 +0800

    feat(entity): add some log while insert visit records succeed

--- crates/entity/src/helper/visitor.rs
+++ crates/entity/src/helper/visitor.rs
@@ -1,5 +1,5 @@
 use sea_orm::{ConnectionTrait, DbErr, EntityTrait, PaginatorTrait};
-use tracing::error;
+use tracing::{error, info};
 
 use crate::model::{prelude::*, visit_record::ActiveModel};
 
@@ -13,7 +13,8 @@ pub trait VisitRecordHelper {
         let _ = VisitRecord::insert_many(ele)
             .exec_without_returning(db)
             .await
-            .inspect_err(|err| error!("failed to insert records: {err}"));
+            .inspect_err(|err| error!("failed to insert records: {err}"))
+            .inspect(|r| info!("insert {r} visit records into database"));
     }
 
     async fn get_count<C>(db: &C) -> Result<u64, DbErr>
